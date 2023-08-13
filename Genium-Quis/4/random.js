53d8c8db32081339627f4f5746ee3090035e6e19
Israel Henrique <138731060+Israel-Henrique@users.noreply.github.com>
2023-08-13 16:21:51 -0300

    Pergunta 4
    Pergunta 4 feita, e já marquei a 5.

--- Genium-Quis/4/random.js
+++ Genium-Quis/4/random.js
@@ -1,4 +1,4 @@
-let caminhos = ["2-sabe-muito", "3", "2-errou", "2-errou"];
+let caminhos = ["5", "4-sombra", "4-sonic-preto", "link"];
 window.onload = () => 
 {
     let opcao = document.getElementsByClassName("opcao");
@@ -7,6 +7,39 @@ window.onload = () =>
         opcao[op].onclick = () => {window.location.replace("https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
         `${caminhos[op]}` + "/genium.html")}
     }
+
+    opcao[3].onclick = () => {window.location.replace("https://www.urbandictionary.com/define.php?term=Edgy%20the%20Hedgy");}
+    opcao[0].onclick = () => {
+        document.getElementsByTagName("img")[0].src = "./recursos/shash.png";
+        document.getElementsByTagName("p")[0].innerHTML = "Voce sente o Shadow no seu...";
+        document.getElementsByTagName("h1")[0].innerHTML = "VOCÊ ACERTOU!";
+
+        for (let op = 0; op < opcao.length; op++)
+        {
+            opcao[op].style.cursor = "default";
+            opcao[op].style.backgroundColor = "#c6c6c6";
+        }
+
+        opcao[0].style.backgroundColor = "#eeeeee";
+
+        
+        let agr = document.createElement("a");
+        agr.href = "https://funnyjunk.com/Edgy+the+hedgy/ungjMha/";
+        document.querySelector(".conteudo").insertBefore(agr, document.querySelector("img"));
+        agr.appendChild(document.querySelector("img"));
+
+        let proxima = document.createElement("div");
+        proxima.classList.add("opcao");
+        let parag = document.createElement("p");
+        parag.innerHTML = "PROXIMA PERGUNTA";
+        proxima.appendChild(parag);
+        proxima.onclick = () => {
+            window.location.replace("https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+            `${caminhos[0]}` + "/genium.html");
+        }
+        document.querySelector(".conteudo").insertBefore(proxima, document.querySelector("section"));
+    }
+
 
     document.getElementsByClassName("debug")[0].onclick = () => {debug();}
 }
---
+++ Genium-Quis/5/random.js
@@ -0,0 +1,28 @@
+let caminhos = ["0-que-isso", "0-mais-o-que-fazer", "0-afim", "1"];
+window.onload = () => 
+{
+    let opcao = document.getElementsByClassName("opcao");
+    for (let op = 0; op < opcao.length; op++)
+    {
+        opcao[op].onclick = () => {window.location.replace("https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+        `${caminhos[op]}` + "/genium.html")}
+    }
+
+    document.getElementsByClassName("debug")[0].onclick = () => {debug();}
+}
+
+function debug () 
+{
+    let opcao = document.getElementsByClassName("opcao");
+    for (let op = 0; op < opcao.length; op++)
+    {
+        opcao[op].onclick = () => {window.location.href = "https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+        `${caminhos[op]}` + "/genium.html";}
+    }
+}
+
+function voltar()
+{
+    window.location.href = "https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+    "0"+ "/genium.html";
+}
