b53d9aa27cb7599816a0a46548343f414d6db791
Israel Henrique <138731060+Israel-Henrique@users.noreply.github.com>
2023-08-25 18:42:40 -0300

    Pergunta 7
    Pergunta 7 feita, pergunta de dragon ball

---
+++ Genium-Quis/7/random.js
@@ -0,0 +1,28 @@
+let caminhos = ["7-kuririn", "7-errou", "7-faz-o-L", "8"];
+window.onload = () => 
+{
+    let opcao = document.getElementsByClassName("opcao");
+    for (let op = 0; op < opcao.length; op++)
+    {
+        opcao[op].onclick = () => {window.location.replace("https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+        `${caminhos[op]}` + "/genium.html")}
+    }
+
+    document.getElementsByClassName("debug")[0].onclick = () => {debug();}
+}
+
+function debug () 
+{
+    let opcao = document.getElementsByClassName("opcao");
+    for (let op = 0; op < opcao.length; op++)
+    {
+        opcao[op].onclick = () => {window.location.href = "https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+        `${caminhos[op]}` + "/genium.html";}
+    }
+}
+
+function voltar()
+{
+    window.location.href = "https://israel-henrique.github.io/66-perguntas-dos-desafios/Genium-Quis/" + 
+    "0"+ "/genium.html";
+}
